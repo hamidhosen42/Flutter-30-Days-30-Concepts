f739d3dbe9f15bb495be699fab8a86ab33fcd991
Md.Hamid Hosen <68488154+hamidhosen42@users.noreply.github.com>
2022-02-04 09:58:35 +0600

    Add files via upload

--- lib/widget/10-Bottom Sheet Widget in Flutter day10.dart
+++ lib/widget/10-Bottom Sheet Widget in Flutter day10.dart
@@ -1,15 +1,59 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
-class BottomSheet extends StatelessWidget {
-  const BottomSheet({ Key? key }) : super(key: key);
+class BottomSheetWidget extends StatelessWidget {
+  const BottomSheetWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Bottom Sheet Widget in Flutter"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text("BottomSheetWidget"),
+          onPressed: () {
+            showModalBottomSheet(
+                elevation: 0,
+                isScrollControlled: true,
+                isDismissible: false,
+                enableDrag: true,
+                backgroundColor: Colors.green,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                  ),
+                ),
+                context: context,
+                builder: (context) {
+                  return Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      ListTile(
+                        title: Text("Hamid Hosen"),
+                        subtitle: Text("EDU"),
+                      ),
+                      ListTile(
+                        title: Text("Hamid Hosen"),
+                        subtitle: Text("EDU"),
+                      ),
+                      ListTile(
+                        title: Text("Hamid Hosen"),
+                        subtitle: Text("EDU"),
+                      ),
+                      ListTile(
+                        title: Text("Hamid Hosen"),
+                        subtitle: Text("EDU"),
+                      ),
+                    ],
+                  );
+                });
+          },
+        ),
       ),
     );
   }
