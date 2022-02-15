bcd69923348aa489fc1949c8a7236efd613c469d
Md.Hamid Hosen <68488154+hamidhosen42@users.noreply.github.com>
2022-02-16 00:14:46 +0600

    Add files via upload

---
+++ lib/widget/21-Flutter App Logos and Splash Screen day21.dart
@@ -0,0 +1,26 @@
+// app logo luncher: flutter_launcher_icons: ^0.9.2
+
+// 1. Setup the config file
+
+/* dev_dependencies:
+  flutter_launcher_icons: "^0.9.2"
+
+flutter_icons:
+  android: "launcher_icon"
+  ios: true
+  image_path: "assets/icon/icon.png" */
+
+// 2. Run the package
+
+/* flutter pub get
+flutter pub run flutter_launcher_icons:main */
+
+// -------------------Splash Screen------------------
+/* flutter_native_splash:
+  color: "#42a5f5"
+  image: "assets/img1.png"
+  android: true
+  ios: true */
+
+// 2. Run the package
+// flutter pub run flutter_native_splash:create
